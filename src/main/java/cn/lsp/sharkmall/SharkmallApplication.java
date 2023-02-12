bfcccca7ffb7876785f7d1498b34b334fd5d9624
sweeterjava <sweeterjava@163.com>
2023-02-12 21:56:37 +0800

    Initial commit

---
+++ src/main/java/cn/lsp/sharkmall/SharkmallApplication.java
@@ -0,0 +1,13 @@
+package cn.lsp.sharkmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharkmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lsp/sharkmall/SharkmallApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lsp.sharkmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
